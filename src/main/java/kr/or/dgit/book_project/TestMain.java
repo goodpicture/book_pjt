0acb62d92e09c27bfb2574c3e4d323c5a02db5df
osram21 <osram21@naver.com>
2017-03-27 13:10:37 +0900

    exerd 추가및 sql 완성

--- src/main/java/kr/or/dgit/book_project/TestMain.java
+++ src/main/java/kr/or/dgit/book_project/TestMain.java
@@ -6,6 +6,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
 
 public class TestMain extends JFrame {
 
@@ -35,8 +37,14 @@ public class TestMain extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel);
+		
+		JLabel lblNewLabel = new JLabel("ㅎㅇ");
+		panel.add(lblNewLabel);
 	}
 
 }
